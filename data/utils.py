bac4e08a8eeb436015428240ffa03c99cb241e2c
ilovemyminutes <silkstaff@naver.com>
2023-11-25 18:32:13 +0900

    feature: add constant variables

--- data/utils.py
+++ data/utils.py
@@ -4,3 +4,11 @@ COL_USER_ID = "userId"
 COL_ITEM_ID = "movieId"
 COL_ITEM_VALUE = "rating"
 COL_TIMESTAMP = "timestamp"
+
+TOKEN_PAD = "[PAD]"
+TOKEN_MASK = "[MASK]"
+TOKEN_CLS = "[CLS]"
+
+TRAIN_DIR = "train"
+VALID_DIR = "valid"
+TEST_DIR = "test"
