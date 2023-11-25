a495eff6e31105ffb9f74de0cbf32537306adeab
ilovemyminutes <silkstaff@naver.com>
2023-11-25 19:36:26 +0900

    feature: add tqdm dependency

--- data/builder.py
+++ data/builder.py
@@ -1,9 +1,13 @@
 from abc import abstractmethod, ABCMeta
 from datetime import datetime, timedelta
 from pathlib import Path
+import math
 
 import pandas as pd
 import numpy as np
+import ray
+from tqdm import tqdm
+import shutil
 
 from data.utils import (
     RAW_FILE,
@@ -16,12 +20,18 @@ from data.utils import (
     TOKEN_MASK,
 )
 
+USER_FILE = "users.txt"
+ITEM_IDS_FILE = "item_ids.txt"
+VALUES_FILE = "values.txt"
+TIMESTAMPS_FILE = "timestamps.txt"
+BEHAVIOR_DATA_FILES = [USER_FILE, ITEM_IDS_FILE, VALUES_FILE, TIMESTAMPS_FILE]
+
 
 class DataBuilder(metaclass=ABCMeta):
     def build(self):
         self.collect()
         self.initialize_tokenizers()
-        self.make_dataset()
+        self.build_dataset()
         self.finalize()
 
     @abstractmethod
@@ -33,7 +43,7 @@ class DataBuilder(metaclass=ABCMeta):
         raise NotImplementedError
 
     @abstractmethod
-    def make_dataset(self):
+    def build_dataset(self):
         raise NotImplementedError
 
     @abstractmethod
@@ -58,7 +68,7 @@ class BehaviorDataBuilder:
         test_log_start: datetime | None = None,
         num_items: int = 20000,
         rating_scale: int = 10,
-        n_jobs: int = 4
+        n_jobs: int = 4,
     ):
         self.data_dir = data_dir
         self.save_dir = save_dir
@@ -73,7 +83,7 @@ class BehaviorDataBuilder:
 
         self.num_items = num_items
         self.rating_scale = rating_scale
-        self.n_job = n_jobs
+        self.n_jobs = n_jobs
 
         self.raw_data: pd.DataFrame | None = None
         self.item_tokenizer: dict[int, int] | None = None
@@ -88,14 +98,31 @@ class BehaviorDataBuilder:
         self._initialize_item_tokenizer()
         self._initialize_value_tokenizer()
 
-    def make_dataset(self, period: tuple[datetime, datetime] | None):
+    def build_dataset(self, period: tuple[datetime, datetime] | None):
+        self.save_dir.mkdir(exist_ok=True, parents=True)
+
+        ray.init(num_cpus=4, runtime_env={"working_dir": "./"})
         source = self.raw_data[
             (period[0].timestamp() <= self.raw_data[COL_TIMESTAMP])
             & (self.raw_data[COL_TIMESTAMP] <= period[1].timestamp())
             & (self.raw_data[COL_ITEM_ID].isin(self.item_tokenizer))
             & (self.raw_data[COL_ITEM_VALUE].isin(self.value_tokenizer))
         ]
-        return source
+        source_ref: ray.ObjectRef = ray.put(source)
+        item_tokenizer_ref: ray.ObjectRef = ray.put(self.item_tokenizer)
+        value_tokenizer_ref: ray.ObjectRef = ray.put(self.value_tokenizer)
+
+        futures = [
+            self._build_dataset_by_one_process.remote(
+                source_ref, split_user_pool, item_tokenizer_ref, value_tokenizer_ref, self.save_dir / f"{job_id}"
+            )
+            for job_id, split_user_pool in enumerate(
+                self._split_user_pool(set(source[COL_USER_ID]), n_splits=self.n_jobs)
+            )
+        ]
+        partition_dirs = ray.get(futures)
+        ray.shutdown()
+        self._merge_partitioned_datasets(partition_dirs, self.save_dir)
 
     def _initialize_item_tokenizer(self):
         self.item_tokenizer = {TOKEN_PAD: 0, TOKEN_MASK: 1, TOKEN_CLS: 2}
@@ -120,3 +147,44 @@ class BehaviorDataBuilder:
                 for i, value in enumerate(range(1, self.rating_scale + 1), start=len(self.value_tokenizer))
             }
         )
+
+    @staticmethod
+    def _split_user_pool(user_pool: set[int], n_splits: int) -> list[set[int]]:
+        user_list = list(user_pool)
+        subset_size = math.ceil(len(user_pool) / n_splits)
+        splits = [set(user_list[k * subset_size : (k + 1) * subset_size]) for k in range(n_splits)]
+        return splits
+
+    @staticmethod
+    @ray.remote
+    def _build_dataset_by_one_process(
+        source: ray.ObjectRef,
+        user_pool: set[int],
+        item_tokenizer: ray.ObjectRef,
+        value_tokenizer: ray.ObjectRef,
+        save_dir: Path,
+    ):
+        save_dir.mkdir(exist_ok=True, parents=True)
+        source = source[source[COL_USER_ID].isin(user_pool)].reset_index(drop=True)
+        source[COL_ITEM_ID] = source[COL_ITEM_ID].map(item_tokenizer)
+        source[COL_ITEM_VALUE] = source[COL_ITEM_VALUE].map(value_tokenizer)
+        files = [open(save_dir / f, "w") for f in BEHAVIOR_DATA_FILES]
+        for u, sequences in tqdm(
+            source.groupby(COL_USER_ID)[[COL_ITEM_ID, COL_ITEM_VALUE, COL_TIMESTAMP]].agg(list),
+            total=source[COL_USER_ID].nunique(),
+            desc="build dataset",
+        ):
+            data = [u] + [",".join(str(x) for x in seq) for seq in sequences]
+            for f, d in zip(files, data):
+                f.write(f"{d}\n")
+        return save_dir
+
+    @staticmethod
+    def _merge_partitioned_datasets(partition_dirs: list[Path], save_dir: Path):
+        for d in tqdm(BEHAVIOR_DATA_FILES, desc="merge partitioned datasets"):
+            f = open(save_dir / d, "w")
+            for p_dir in partition_dirs:
+                for v in open(p_dir / d, "r"):
+                    f.write(v)
+        for p_dir in partition_dirs:
+            shutil.rmtree(p_dir)
